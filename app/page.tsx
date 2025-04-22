9f7442b0d6b53a2c2ae5d5c8e2d5bbc158d00ffd
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-04-22 11:20:26 +0530

    added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { HeroSection } from "@/components/heroSection";
 import { Header } from "@/components/ui/header";
 
 export default function Home() {
   return <div className="flex flex-col min-h-screen">
     <Header />
+    <HeroSection />
   </div>
 }
