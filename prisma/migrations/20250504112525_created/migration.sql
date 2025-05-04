a0c59ea646a1869f5c027d8b0ac3ecfe5f965fee
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-05-04 16:57:07 +0530

    created Feedback table

---
+++ prisma/migrations/20250504112525_created/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" TEXT NOT NULL,
+    "interviewId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "totalScore" INTEGER NOT NULL,
+    "categoryScores" INTEGER NOT NULL,
+    "areasForImprovement" TEXT NOT NULL,
+    "finalAssessment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+);
